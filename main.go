eab58a148caf97f883072e84b45168e00e35f6f8
danielr <daniel.ratmiroff@sdui.de>
2024-09-09 12:45:52 +0200

    init

--- main.go
+++ main.go
@@ -2,28 +2,56 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+
+	"github.com/Danielratmiroff/pretty-fzf/config"
+	"github.com/Danielratmiroff/pretty-fzf/fzf"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "Pretty fzf",
+	Short: "Pretty fzf wrapper",
+	Long:  "Pretty fzf wrapper long",
+	Run: func(cmd *cobra.Command, args []string) {
+		if runFlag, _ := cmd.Flags().GetBool("run"); runFlag {
+			fmt.Println("Running the prompt...")
+		}
+
+		params := fzf.NewDefaultConfig()
+
+		err := fzf.RunCommand(params)
+
+		if err != nil {
+			log.Fatalf("Error running fzf: %v", err)
+		}
+
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Register flags
+	rootCmd.Flags().BoolP("run", "r", false, "Execute the prompt")
+
+	// Bind flags to viper
+	viper.BindPFlag("run", rootCmd.Flags().Lookup("run"))
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		printHelp()
-		return
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
 	}
 
-	command := os.Args[1]
-
-	switch command {
-	default:
-		printHelp()
+	config, err := config.LoadConfig()
+	if err != nil {
+		panic(err)
 	}
-}
 
-func printHelp() {
-	fmt.Println("Usage:")
-	fmt.Println("  [command]")
-}
-
-func printVersion() {
-	fmt.Println("Version: 1.0.0")
+	fmt.Printf("Var1: %s\n", config.Var1)
+	fmt.Printf("Var2: %s\n", config.Var2)
+	fmt.Printf("Var3: %s\n", config.Var3)
 }
